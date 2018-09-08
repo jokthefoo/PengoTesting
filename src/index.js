2fd695b578ffd4c8a414b58a3f2518bd339ff14e
Alex Kofford <alexkofford@gmail.com>
2018-09-08 15:03:02 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import 'phaser';
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('logo', 'assets/logo.png');
+    
+}
+
+function create ()
+{
+    var logo = this.add.image(400, 150, 'logo');
+    Phaser.Actions.gr
+    this.tweens.add({
+        targets: logo,
+        y: 450,
+        duration: 2000,
+        ease: 'Power2',
+        yoyo: true,
+        loop: -1
+    });
+
+}
+
+function update ()
+{
+
+}
